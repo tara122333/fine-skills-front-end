f30012dc483823c7ca8a8029211cb9e5e0fc2bce
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2022-08-18 15:19:42 +0530

    update hero button

--- src/page/Home.page.jsx
+++ src/page/Home.page.jsx
@@ -19,7 +19,7 @@ const HomePage = ()=>{
     return(
         <>
             <div className="flex gap-7 flex-col">
-                <div className="flex flex-col md:flex-row-reverse md:justify-between md:container md:mx-auto md:px-10 md:mt-10 my-10 overflow-hidden">
+                <div className="flex flex-col md:flex-row-reverse md:justify-between md:container md:mx-auto md:px-10 md:mt-10 my-5 md:my-16 overflow-hidden">
                     <div className="w-full md:w-1/2 h-full relative">
                         <div className="px-3 py-2 lg:py-8 md:px-20 md:py-5">
                             <img src={img} alt="hometheme" className="w-full h-full" />
@@ -46,13 +46,13 @@ const HomePage = ()=>{
                         </div>
                         <div className="w-full flex flex-col gap-3 px-2 md:px-0 md:my-5 md:flex-row">
                             <a href=" https://chat.whatsapp.com/GrD14ONRd8RKRbLKCCF4V2" target="_blank" className="w-full bg-tara-0 rounded-full">
-                                <div className="flex  justify-center items-center gap-3 md:gap-6 bg-gradient-to-r from-tara-yellow to-tara-orange rounded-full px-8 py-2 text-white shadow-2xl shadow-tara-yellow">
+                                <div className="flex  justify-center items-center gap-3 md:gap-6 bg-gradient-to-r from-tara-yellow to-tara-orange rounded-full px-8 py-2 text-white shadow-2xl shadow-tara-yellow hover:bg-gradient-to-r hover:from-tara-orange hover:to-tara-yellow">
                                     <BsWhatsapp  className="text-tara-1 text-xl"/>
                                     <span className="text-tara-1 text-lg">Join WhatsApp</span>
                                 </div>
                             </a>
                             <a href=" https://discord.gg/kJESc7gA" target="_blank" className="w-full bg-tara-0 rounded-full cursor-pointer">
-                                <div className="bg-gradient-to-r from-tara-yellow to-tara-orange rounded-full px-8 py-2 text-white shadow-2xl shadow-tara-yellow flex justify-center items-center gap-3 md:gap-6 ">
+                                <div className="bg-gradient-to-r from-tara-yellow to-tara-orange rounded-full px-8 py-2 text-white shadow-2xl shadow-tara-yellow flex justify-center items-center gap-3 md:gap-6 hover:bg-gradient-to-r hover:from-tara-orange hover:to-tara-yellow">
                                     <FaDiscord  className="text-tara-1 text-xl"/>
                                     <span className="text-tara-1 text-lg">Join Discord</span>
                                 </div>
