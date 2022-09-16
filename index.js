d00de8b7657ff044bd394c1118e347569402fc24
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2022-09-16 17:20:07 +0530

    change in json

---
+++ index.js
@@ -0,0 +1,9 @@
+
+  "scripts": {
+    "predeploy": "npm run build",
+    "deploy": "gh-pages -d build",
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject",
+    "production": "cross-env NODE_ENV=production node_modules/webpack/bin/webpack.js --no-progress --hide-modules --config=node_modules/laravel-mix/setup/webpack.config.js"
